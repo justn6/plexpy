3d3ca3623a1ee3e8622f8ad0aa23d720a31c9ea9
justn6 <justn6@gmail.com>
2017-03-28 09:23:04 +0000

    published by M15X
    published by M15X

--- plexpy/__init__.py
+++ plexpy/__init__.py
@@ -1,7 +1,7 @@
 ﻿# This file is part of PlexPy.
 #
 #  PlexPy is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
+#  it under the terms of the GNU General Public License as published by M15X
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
